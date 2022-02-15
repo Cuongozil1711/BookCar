85805b21ad6008c074915c7c7f02a615efecc3da
NguyenVietCuong <Cuongptit1711@gmail.com>
2022-02-15 22:18:11 +0700

    cuong

---
+++ nv6/App.js
@@ -0,0 +1,24 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import HomeScreen from './screen/HomeScreen';
+import DetailsScreen from './screen/DetailScreen';
+
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Details" component={DetailsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
